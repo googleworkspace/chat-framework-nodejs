07990d04bd6ce8c5dcfb0b20958312c8a915d6d9
Steven Bazyl <sbazyl@google.com>
2021-11-11 12:41:15 -0700

    Fix typo in dir name + make version compat with semver

--- packages/dialogflow-es/jest.config.js
+++ packages/dialogflow-es/jest.config.js
@@ -3,6 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    "^jose/(.*)$": "<rootDir>/../../node_modules/jose/dist/node/cjs/$1",
+    '^jose/(.*)$': '<rootDir>/../../node_modules/jose/dist/node/cjs/$1',
   },
 };
